85d4da219f7a22ee01eca8c91930c8e0308af97f
Matt Olsen <matt@digital-wanderlust.com>
2016-02-06 22:53:54 -0800

    add parentdir transform

---
+++ pathlt/transforms.py
@@ -0,0 +1,26 @@
+from __future__ import (absolute_import, division, generators, nested_scopes,
+                        print_function,unicode_literals, with_statement)
+
+import re
+import os
+
+
+def parentdir_expand(path):
+  """ Expand parent directory traversal
+
+  Allow the user to specify ..<n> and expand to a recognized directory path.
+  for example ..5 would expand to ../../../../..
+
+  :param path: str
+  :return: str
+  """
+  if path[0:2] != '..':
+    return path
+  else:
+    parentdir_match = r'\.\.(\d*){}(.*)'.format(os.path.sep)
+    matches = re.match(parentdir_match, path)
+    expand_depth = matches.group(1) or 1
+    extra_path = matches.group(2)
+
+    parent_path = os.path.join(*['..'] * int(expand_depth))
+    return os.path.join(parent_path, extra_path)
---
+++ tests/transforms_tests.py
@@ -0,0 +1,17 @@
+from __future__ import (absolute_import, division, generators, nested_scopes,
+                        print_function, unicode_literals, with_statement)
+
+import pathlt.transforms
+
+
+def test_no_parents():
+  input = 'test/1'
+  output = pathlt.transforms.parentdir_expand(input)
+  assert input == output
+
+
+def test_expand_parents():
+  input = '..2/test'
+  output = pathlt.transforms.parentdir_expand(input)
+  print(output)
+  assert output == '../../test'
